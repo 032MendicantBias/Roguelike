7abc650e174a96db58f3625e8c8c117fcd2c594d
JamesChristen <james.christen@hotmail.co.uk>
2016-07-30 13:58:29 +0100

    Rectangle  collisions

--- RogueLike/Game1/TestPhysics.cs
+++ RogueLike/Game1/TestPhysics.cs
@@ -11,31 +11,71 @@ namespace TestRogueLike
         [TestMethod]
         public void TestOutOfRange()
         {
-            Rectangle rectA = new Rectangle(0, 10, 10, 10);
+            Rectangle rectA = new Rectangle(0, 0, 10, 10);
             PhysicsObject po = new PhysicsObject(rectA);
 
-            Rectangle rectB = new Rectangle(100, 100, 1, 1);
-            Assert.AreEqual(false, po.CollidedRectangle(rectB));
+            Rectangle rectB = new Rectangle(100, 100, 10, 10);
+            Assert.IsFalse(po.CollidedRectangle(rectB));
         }
 
         [TestMethod]
         public void TestVertInRange()
         {
-            Rectangle rectA = new Rectangle(0, 10, 10, 10);
+            Rectangle rectA = new Rectangle(0, 0, 10, 10);
             PhysicsObject po = new PhysicsObject(rectA);
 
-            Rectangle rectB = new Rectangle(100, 100, 1, 1);
-            Assert.AreEqual(false, po.CollidedRectangle(rectB));
+            Rectangle rectB = new Rectangle(100, 0, 10, 10);
+            Assert.IsFalse(po.CollidedRectangle(rectB));
         }
 
         [TestMethod]
         public void TestHoriInRange()
         {
+            Rectangle rectA = new Rectangle(0, 0, 10, 10);
+            PhysicsObject po = new PhysicsObject(rectA);
+
+            Rectangle rectB = new Rectangle(0, 100, 10, 10);
+            Assert.IsFalse(po.CollidedRectangle(rectB));
+        }
+
+        [TestMethod]
+        public void TestInRange()
+        {
             Rectangle rectA = new Rectangle(0, 10, 10, 10);
             PhysicsObject po = new PhysicsObject(rectA);
 
-            Rectangle rectB = new Rectangle(100, 100, 1, 1);
-            Assert.AreEqual(false, po.CollidedRectangle(rectB));
+            Rectangle rectB = new Rectangle(0, 5, 10, 10);
+            Assert.IsTrue(po.CollidedRectangle(rectB));
+        }
+
+        [TestMethod]
+        public void TestClippedVert()
+        {
+            Rectangle rectA = new Rectangle(0, 0, 10, 10);
+            PhysicsObject po = new PhysicsObject(rectA);
+
+            Rectangle rectB = new Rectangle(0, 10, 10, 10);
+            Assert.IsTrue(po.CollidedRectangle(rectB));
+        }
+
+        [TestMethod]
+        public void TestClippedHori()
+        {
+            Rectangle rectA = new Rectangle(0, 0, 10, 10);
+            PhysicsObject po = new PhysicsObject(rectA);
+
+            Rectangle rectB = new Rectangle(10, 0, 10, 10);
+            Assert.IsTrue(po.CollidedRectangle(rectB));
+        }
+
+        [TestMethod]
+        public void TestCornerHit()
+        {
+            Rectangle rectA = new Rectangle(0, 0, 10, 10);
+            PhysicsObject po = new PhysicsObject(rectA);
+
+            Rectangle rectB = new Rectangle(10, 10, 10, 10);
+            Assert.IsTrue(po.CollidedRectangle(rectB));
         }
     }
 }
--- RogueLike/RogueLike/PhysicsObject.cs
+++ RogueLike/RogueLike/PhysicsObject.cs
@@ -16,13 +16,45 @@ namespace RogueLike.Physics
 
             Vector2 positionDiff = Vector2.Subtract(rect.GetCentre(), rectIn.GetCentre());
 
-            if (Math.Abs(positionDiff.X) < (rect.Width / 2) + (rectIn.Width / 2))
+            if (Math.Abs(positionDiff.X) <= (rect.Width / 2) + (rectIn.Width / 2))
+            {
                 inRangeHori = true;
+            }
 
-            if (Math.Abs(positionDiff.Y) < (rect.Height / 2) + (rectIn.Height / 2))
+            if (Math.Abs(positionDiff.Y) <= (rect.Height / 2) + (rectIn.Height / 2))
+            {
                 inRangeVert = true;
+            }
 
             return (inRangeVert && inRangeHori);
         }
+
+        public bool CollidedCircle(Circle circ)
+        {
+            bool inRangeVert = false;
+            bool inRangeHori = false;
+
+
+
+            return (inRangeVert && inRangeHori);
+        }
+    }
+
+    public class Circle
+    {
+        private Vector2 Position { get; set; }
+        private float Radius { get; set; }
+
+        public Circle(Vector2 Position, float Radius)
+        {
+            this.Position = Position;
+            this.Radius = Radius;
+        }
+
+        public Vector2 GetCentre()
+        {
+            return Position;
+        }
+
     }
 }
