430d63a901f3d6c85e89f758736996f761f99584
JamesChristen <james.christen@hotmail.co.uk>
2016-07-30 17:22:39 +0100

    Physics changes

--- RogueLike/RogueLike/PhysicsObject.cs
+++ RogueLike/RogueLike/PhysicsObject.cs
@@ -31,29 +31,33 @@ namespace RogueLike.Physics
 
         public bool CollidedCircle(Circle circ)
         {
-            bool inRangeVert = false;
-            bool inRangeHori = false;
+            Vector2 positionDiff = Vector2.Subtract(rect.GetCentre(), circ.Position);
+            positionDiff.X = Math.Abs(positionDiff.X);
+            positionDiff.Y = Math.Abs(positionDiff.Y);
 
+            if (positionDiff.X > (rect.Width / 2) + circ.Radius) { return false; }
+            if (positionDiff.Y > (rect.Height / 2) + circ.Radius) { return false; }
 
+            if (positionDiff.X <= (rect.Width / 2)) { return true; }
+            if (positionDiff.Y <= (rect.Height / 2)) { return true; }
 
-            return (inRangeVert && inRangeHori);
+            float cornerDistSquared = (float)Math.Pow((positionDiff.X - (rect.Width / 2)), 2)
+                + (float)Math.Pow((positionDiff.Y - (rect.Height / 2)), 2);
+
+            return cornerDistSquared <= (float) Math.Pow(circ.Radius, 2);
         }
     }
 
+
     public class Circle
     {
-        private Vector2 Position { get; set; }
-        private float Radius { get; set; }
+        public Vector2 Position { get; set; }
+        public float Radius { get; set; }
 
         public Circle(Vector2 Position, float Radius)
         {
             this.Position = Position;
             this.Radius = Radius;
-        }
-
-        public Vector2 GetCentre()
-        {
-            return Position;
         }
 
     }
--- RogueLike/TestRogueLike/TestPhysics.cs
+++ RogueLike/TestRogueLike/TestPhysics.cs
@@ -4,6 +4,7 @@ using RogueLike.Physics;
 
 namespace TestRogueLike
 {
+
     [TestClass]
     public class PhysicsObjectRectangleCollisionTest
     {
@@ -75,6 +76,170 @@ namespace TestRogueLike
 
             Rectangle rectB = new Rectangle(10, 10, 10, 10);
             Assert.IsTrue(po.CollidedRectangle(rectB));
+        }
+    }
+
+    [TestClass]
+    public class PhysicsObjectCircleCollisionTest
+    {
+        [TestMethod]
+        public void TestOutOfRange()
+        {
+            Rectangle rect = new Rectangle(0, 0, 10, 10);
+            PhysicsObject po = new PhysicsObject(rect);
+
+            Circle circ = new Circle(new Vector2(100, 100), 10);
+            Assert.IsFalse(po.CollidedCircle(circ));
+        }
+
+        [TestMethod]
+        public void TestVertInRange()
+        {
+            Rectangle rect = new Rectangle(0, 0, 10, 10);
+            PhysicsObject po = new PhysicsObject(rect);
+
+            Circle circ = new Circle(new Vector2(0, 100), 10);
+            Assert.IsFalse(po.CollidedCircle(circ));
+        }
+
+        [TestMethod]
+        public void TestHoriInRange()
+        {
+            Rectangle rect = new Rectangle(0, 0, 10, 10);
+            PhysicsObject po = new PhysicsObject(rect);
+
+            Circle circ = new Circle(new Vector2(100, 0), 10);
+            Assert.IsFalse(po.CollidedCircle(circ));
+        }
+
+        [TestMethod]
+        public void TestCollisionTop()
+        {
+            Rectangle rect = new Rectangle(0, 0, 10, 10);
+            PhysicsObject po = new PhysicsObject(rect);
+
+            Circle circ = new Circle(new Vector2(5, -5), 10);
+            Assert.IsTrue(po.CollidedCircle(circ));
+        }
+
+        [TestMethod]
+        public void TestCollisionBottom()
+        {
+            Rectangle rect = new Rectangle(0, 0, 10, 10);
+            PhysicsObject po = new PhysicsObject(rect);
+
+            Circle circ = new Circle(new Vector2(5, 15), 10);
+            Assert.IsTrue(po.CollidedCircle(circ));
+        }
+
+        [TestMethod]
+        public void TestCollisionLeft()
+        {
+            Rectangle rect = new Rectangle(0, 0, 10, 10);
+            PhysicsObject po = new PhysicsObject(rect);
+
+            Circle circ = new Circle(new Vector2(-5, 5), 10);
+            Assert.IsTrue(po.CollidedCircle(circ));
+        }
+
+        [TestMethod]
+        public void TestCollisionRight()
+        {
+            Rectangle rect = new Rectangle(0, 0, 10, 10);
+            PhysicsObject po = new PhysicsObject(rect);
+
+            Circle circ = new Circle(new Vector2(15, 5), 10);
+            Assert.IsTrue(po.CollidedCircle(circ));
+        }
+
+        [TestMethod]
+        public void TestCollisionInside()
+        {
+            Rectangle rect = new Rectangle(0, 0, 10, 10);
+            PhysicsObject po = new PhysicsObject(rect);
+
+            Circle circ = new Circle(new Vector2(5, 5), 5);
+            Assert.IsTrue(po.CollidedCircle(circ));
+        }
+
+        [TestMethod]
+        public void TestOutOfRangeTopLeft()
+        {
+            Rectangle rect = new Rectangle(0, 0, 10, 10);
+            PhysicsObject po = new PhysicsObject(rect);
+
+            Circle circ = new Circle(new Vector2(-5, -5), 5);
+            Assert.IsFalse(po.CollidedCircle(circ));
+        }
+
+        [TestMethod]
+        public void TestCollisionTopLeft()
+        {
+            Rectangle rect = new Rectangle(0, 0, 10, 10);
+            PhysicsObject po = new PhysicsObject(rect);
+
+            Circle circ = new Circle(new Vector2(-1, -1), 5);
+            Assert.IsTrue(po.CollidedCircle(circ));
+        }
+
+        [TestMethod]
+        public void TestOutOfRangeBottomLeft()
+        {
+            Rectangle rect = new Rectangle(0, 0, 10, 10);
+            PhysicsObject po = new PhysicsObject(rect);
+
+            Circle circ = new Circle(new Vector2(-5, 15), 5);
+            Assert.IsFalse(po.CollidedCircle(circ));
+        }
+
+        [TestMethod]
+        public void TestCollisionBottomLeft()
+        {
+            Rectangle rect = new Rectangle(0, 0, 10, 10);
+            PhysicsObject po = new PhysicsObject(rect);
+
+            Circle circ = new Circle(new Vector2(-1, 11), 5);
+            Assert.IsTrue(po.CollidedCircle(circ));
+        }
+
+        [TestMethod]
+        public void TestOutOfRangeTopRight()
+        {
+            Rectangle rect = new Rectangle(0, 0, 10, 10);
+            PhysicsObject po = new PhysicsObject(rect);
+
+            Circle circ = new Circle(new Vector2(15, -5), 5);
+            Assert.IsFalse(po.CollidedCircle(circ));
+        }
+
+        [TestMethod]
+        public void TestCollisionTopRight()
+        {
+            Rectangle rect = new Rectangle(0, 0, 10, 10);
+            PhysicsObject po = new PhysicsObject(rect);
+
+            Circle circ = new Circle(new Vector2(11, -1), 5);
+            Assert.IsTrue(po.CollidedCircle(circ));
+        }
+
+        [TestMethod]
+        public void TestOutOfRangeBottomRight()
+        {
+            Rectangle rect = new Rectangle(0, 0, 10, 10);
+            PhysicsObject po = new PhysicsObject(rect);
+
+            Circle circ = new Circle(new Vector2(-15, 15), 5);
+            Assert.IsFalse(po.CollidedCircle(circ));
+        }
+
+        [TestMethod]
+        public void TestCollisionBottomRight()
+        {
+            Rectangle rect = new Rectangle(0, 0, 10, 10);
+            PhysicsObject po = new PhysicsObject(rect);
+
+            Circle circ = new Circle(new Vector2(11, 11), 5);
+            Assert.IsTrue(po.CollidedCircle(circ));
         }
     }
 }
